6830186bd48a2f13f12368d727b34cc420dc26bc
Siddharth Rao <siddrao.926@gmail.com>
2024-06-14 14:33:58 -0700

    Changed Model Architecture To Handle New Parameters

--- src/training/_build_model.py
+++ src/training/_build_model.py
@@ -1,8 +1,9 @@
 import os
 
 from src.utils import find_project_directory
+from keras.api.models import Model, load_model
 from keras.api.layers import Dense, Conv2D, Dropout, Input, BatchNormalization, \
-    GlobalAveragePooling2D, LeakyReLU, SpatialDropout2D
+    GlobalAveragePooling2D, LeakyReLU, SpatialDropout2D, Concatenate, SeparableConv2D, Add
 
 
 def build_model():
@@ -13,24 +14,57 @@ def build_model():
         model = load_model('models/model.keras')
 
     else:
-        model = Sequential([
-            Input(shape=(8, 8, 12)),
-            Conv2D(64, (3, 3), padding='same'),
-            LeakyReLU(negative_slope=0.1),
-            BatchNormalization(),
-            Conv2D(128, (3, 3), padding='same'),
-            LeakyReLU(negative_slope=0.1),
-            BatchNormalization(),
-            SpatialDropout2D(0.2),
-            Conv2D(256, (3, 3), padding='same'),
-            LeakyReLU(negative_slope=0.1),
-            BatchNormalization(),
-            GlobalAveragePooling2D(),
-            Dense(512),
-            Dropout(0.5),
-            Dense(256),
-            Dense(1, activation='tanh')
-        ])
+        print('Model not found. Building model...')
+        input_board = Input(shape=(8, 8, 12), name='board')
+        input_move_count = Input(shape=(1,), name='move_count')
+        input_to_move = Input(shape=(2,), name='to_move')
+        input_castling_rights = Input(shape=(4,), name='castling_rights')
+        input_has_castled = Input(shape=(4,), name='has_castled')
+        input_material = Input(shape=(10,), name='material')
+        input_features = [input_board, input_move_count, input_to_move, input_castling_rights, input_has_castled,
+                          input_material]
+
+        conv1 = Conv2D(128, (3, 3), padding='same')(input_board)
+        conv1 = BatchNormalization()(conv1)
+        conv1 = LeakyReLU()(conv1)
+
+        conv2 = SeparableConv2D(128, (3, 3), padding='same')(conv1)
+        conv2 = BatchNormalization()(conv2)
+        conv2 = LeakyReLU()(conv2)
+
+        conv3 = SeparableConv2D(128, (3, 3), padding='same')(conv2)
+        conv3 = BatchNormalization()(conv3)
+        conv3 = LeakyReLU()(conv3)
+
+        conv4 = SeparableConv2D(128, (3, 3), padding='same')(conv3)
+        conv4 = BatchNormalization()(conv4)
+        conv4 = LeakyReLU()(conv4)
+
+        residual = Add()([conv1, conv4])
+
+        spatial_dropout = SpatialDropout2D(0.3)(residual)
+        global_pool = GlobalAveragePooling2D()(spatial_dropout)
+
+        combined_features = Concatenate()([global_pool, input_move_count, input_to_move, input_castling_rights,
+                                           input_has_castled, input_material])
+
+        dense1 = Dense(256)(combined_features)
+        dense1 = BatchNormalization()(dense1)
+        dense1 = LeakyReLU()(dense1)
+        dense1 = Dropout(0.5)(dense1)
+
+        dense2 = Dense(256)(dense1)
+        dense2 = BatchNormalization()(dense2)
+        dense2 = LeakyReLU()(dense2)
+        dense2 = Dropout(0.5)(dense2)
+
+        dense3 = Dense(256)(dense2)
+        dense3 = BatchNormalization()(dense3)
+        dense3 = LeakyReLU()(dense3)
+
+        output = Dense(1, activation='tanh')(dense3)
+
+        model = Model(inputs=input_features, outputs=output)
 
     model.compile(
         optimizer='adam',
--- src/training/_train_model.py
+++ src/training/_train_model.py
@@ -1,20 +1,28 @@
 import tensorflow as tf
-from keras.api.models import Sequential
+from keras.api.models import Model
 from keras.api.callbacks import EarlyStopping, ReduceLROnPlateau
 
 
-def fit_model(model, train_data: tf.data.Dataset, epochs=10, batch_size=32, verbose=1):
+def fit_model(model: Model, train_data: tf.data.Dataset, epochs=10, batch_size=32, verbose=1):
     early_stopping = EarlyStopping(
         monitor='loss', patience=5, restore_best_weights=True)
     reduce_lr = ReduceLROnPlateau(
         monitor='loss', factor=0.2, patience=3, min_lr=0.0001)
 
-    model.fit(train_data, epochs=epochs,
-              batch_size=batch_size, verbose=verbose,
-              callbacks=[early_stopping, reduce_lr])
+    boards, move_counts, to_move, castling_rights, has_castled, material, winners = train_data
+
+    model.fit(
+        x=[boards, move_counts, to_move, castling_rights, has_castled, material],
+        y=winners,
+        epochs=epochs,
+        batch_size=batch_size,
+        verbose=verbose,
+        callbacks=[early_stopping, reduce_lr],
+        validation_split=0.1
+    )
 
     return model
 
 
-def save_model(model: Sequential, path: str):
+def save_model(model: Model, path: str):
     model.save(path)
