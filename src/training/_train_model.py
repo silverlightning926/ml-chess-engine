546903efd6b4b3edc8a6b1c3f818473449803615
Siddharth Rao <siddrao.926@gmail.com>
2024-06-16 03:33:42 -0700

    Added Tensorboard Callback
    Fixed Issue #8

--- src/training/_train_model.py
+++ src/training/_train_model.py
@@ -2,7 +2,10 @@ from src.training._load_dataset import BATCH_SIZE
 
 import tensorflow as tf
 from keras.api.models import Model
-from keras.api.callbacks import EarlyStopping, ReduceLROnPlateau
+from keras.api.callbacks import EarlyStopping, ReduceLROnPlateau, TensorBoard
+import datetime as dt
+
+LOG_DIR = 'logs/' + dt.now().strftime("%Y%m%d-%H%M%S")
 
 
 def fit_model(model: Model, train_data: tf.data.Dataset, epochs=10, batch_size=BATCH_SIZE, verbose=1):
@@ -10,13 +13,19 @@ def fit_model(model: Model, train_data: tf.data.Dataset, epochs=10, batch_size=B
         monitor='loss', patience=5, restore_best_weights=True)
     reduce_lr = ReduceLROnPlateau(
         monitor='loss', factor=0.2, patience=3, min_lr=0.0001)
+    tensorboard = TensorBoard(log_dir=LOG_DIR, histogram_freq=1,
+                              write_graph=True, write_images=True, update_freq='batch')
 
     model.fit(
         train_data,
         epochs=epochs,
         batch_size=batch_size,
         verbose=verbose,
-        callbacks=[early_stopping, reduce_lr]
+        callbacks=[
+            early_stopping,
+            reduce_lr,
+            tensorboard
+        ]
     )
 
     return model
