40ef532fd8429416911069a9f09d91cf31842973
Siddharth Rao <siddrao.926@gmail.com>
2024-06-20 22:56:14 -0700

    Changed Model Architecture

--- src/training/_build_model.py
+++ src/training/_build_model.py
@@ -1,26 +1,9 @@
 import os
-from keras.api.models import Model, load_model
-from keras.api.layers import Input, Conv2D, BatchNormalization, LeakyReLU, GlobalAveragePooling2D, Dense, Dropout, TimeDistributed, GRU, Add, LayerNormalization, MultiHeadAttention
-from keras.api.optimizers import Adam
-from keras.api.regularizers import l2
-from keras.api.metrics import Accuracy, CategoricalCrossentropy, AUC, Precision, Recall, MeanSquaredError
+from keras.api.models import Sequential, load_model
+from keras.api.layers import Input, TimeDistributed, Conv2D, MaxPooling2D, Flatten, LSTM, Dropout, Dense, GlobalAveragePooling1D, BatchNormalization
 from src.utils.path_utils import find_project_directory
 
 from src.training._load_dataset import MAX_MOVES, BATCH_SIZE
-
-
-def time_distributed_residual_block(x, filters, kernel_size):
-    shortcut = x
-    x = TimeDistributed(Conv2D(filters, kernel_size,
-                        padding='same', kernel_regularizer=l2(1e-4)))(x)
-    x = TimeDistributed(BatchNormalization())(x)
-    x = TimeDistributed(LeakyReLU())(x)
-    x = TimeDistributed(Conv2D(filters, kernel_size,
-                        padding='same', kernel_regularizer=l2(1e-4)))(x)
-    x = TimeDistributed(BatchNormalization())(x)
-    x = Add()([shortcut, x])
-    x = TimeDistributed(LeakyReLU())(x)
-    return x
 
 
 def build_model():
@@ -32,43 +15,48 @@ def build_model():
     else:
         print('Model not found. Building model...')
 
-        input_layer = Input(shape=(MAX_MOVES, 8, 8, 12),
-                            batch_size=BATCH_SIZE, name='input')
+        model = Sequential([
+            Input(shape=(MAX_MOVES, 8, 8, 12),
+                  batch_size=BATCH_SIZE, name='input'),
 
-        x = TimeDistributed(Conv2D(64, (3, 3), padding='same',
-                            kernel_regularizer=l2(1e-4)))(input_layer)
-        x = TimeDistributed(BatchNormalization())(x)
-        x = TimeDistributed(LeakyReLU())(x)
+            TimeDistributed(
+                Conv2D(64, (3, 3), activation='relu', padding='same')),
+            TimeDistributed(BatchNormalization()),
+            TimeDistributed(MaxPooling2D((2, 2))),
 
-        x = time_distributed_residual_block(x, 64, (3, 3))
-        x = time_distributed_residual_block(x, 64, (3, 3))
+            TimeDistributed(
+                Conv2D(128, (3, 3), activation='relu', padding='same')),
+            TimeDistributed(BatchNormalization()),
+            TimeDistributed(MaxPooling2D((2, 2))),
 
-        x = TimeDistributed(GlobalAveragePooling2D())(x)
+            TimeDistributed(
+                Conv2D(256, (3, 3), activation='relu', padding='same')),
+            TimeDistributed(BatchNormalization()),
+            TimeDistributed(MaxPooling2D((2, 2))),
 
-        x = GRU(256, return_sequences=True,
-                kernel_regularizer=l2(1e-4), stateful=False)(x)
-        x = GRU(256, return_sequences=True,
-                kernel_regularizer=l2(1e-4), stateful=False)(x)
+            TimeDistributed(Flatten()),
 
-        attention_output = MultiHeadAttention(num_heads=4, key_dim=256)(x, x)
-        x = Add()([x, attention_output])
-        x = LayerNormalization()(x)
+            LSTM(256, return_sequences=True),
+            Dropout(0.5),
 
-        x = Dense(128, activation='relu', kernel_regularizer=l2(1e-4))(x)
-        x = Dropout(0.5)(x)
-        x = Dense(128, activation='relu', kernel_regularizer=l2(1e-4))(x)
-        x = Dropout(0.5)(x)
+            TimeDistributed(Dense(512, activation='relu')),
+            TimeDistributed(Dropout(0.5)),
 
-        output_layer = Dense(3, activation='softmax',
-                             kernel_regularizer=l2(1e-4))(x)
+            TimeDistributed(Dense(256, activation='relu')),
+            TimeDistributed(Dropout(0.5)),
 
-        model = Model(inputs=input_layer, outputs=output_layer)
+            TimeDistributed(Dense(128, activation='relu')),
+            TimeDistributed(Dropout(0.5)),
+
+            GlobalAveragePooling1D(),
+
+            Dense(3, activation='softmax')
+        ])
 
         model.compile(
-            optimizer=Adam(learning_rate=1e-4),
+            optimizer='adam',
             loss='categorical_crossentropy',
-            metrics=[Accuracy(), CategoricalCrossentropy(), AUC(), Precision(),
-                     Recall(), MeanSquaredError()]
+            metrics=['accuracy']
         )
 
     return model
